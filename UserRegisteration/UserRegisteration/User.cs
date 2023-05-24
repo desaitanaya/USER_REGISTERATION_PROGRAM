5ca66f7df8299867ec5b0f23e4c16daa8ce4589f
Tanaya <desaitanaya17@gmail.com>
2023-05-24 20:00:21 +0530

    [Tanaya] Add : C# Program to validate First Name in User Registeration Problem.

---
+++ UserRegisteration/UserRegisteration/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegisteration
+{
+    class User
+    {
+        //Validating First Name
+        public void UserFirstName()
+        {
+            Console.WriteLine("Enter First Name : ");
+            string firstName = Console.ReadLine();
+
+            string namePattern = @"^[A-Z]{1}[a-z]{2,}$";
+
+            Regex regexFirstName = new Regex(namePattern);
+
+            if (regexFirstName.IsMatch(firstName))
+            {
+                Console.WriteLine("First Name : " + firstName);
+            }
+            else
+            {
+                Console.WriteLine("Invalid First Name");
+            }
+        }
+    }
+}
