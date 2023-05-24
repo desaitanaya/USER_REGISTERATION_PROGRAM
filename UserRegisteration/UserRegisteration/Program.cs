7046299399aeed743ba8cd0a3f8ca01b460a3a12
Tanaya <desaitanaya17@gmail.com>
2023-05-24 20:00:14 +0530

    [Tanaya] Add : C# Program to validate First Name in User Registeration Problem.

---
+++ UserRegisteration/UserRegisteration/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace UserRegisteration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+
+            user.UserFirstName();
+            
+        }
+    }
+}
